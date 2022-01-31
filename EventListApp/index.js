2aed9a594b7d342b6aafc5f6c60139861829f283
Xiang Liu <jane@Xiangs-MBP.attlocal.net>
2022-01-31 14:06:09 -0800

    Finish Add Feature

--- EventListApp/index.js
+++ EventListApp/index.js
@@ -1,4 +1,3 @@
-
 const Appapi = (() => {
     const baseurl = "http://localhost:3000";
     const path = "events";
@@ -6,37 +5,44 @@ const Appapi = (() => {
     const getTodos = () =>
         fetch([baseurl, path].join("/")).then((response) => response.json())
 
-
+    //AddTodo
     const addTodo = (todo) =>
         fetch([baseurl, path].join("/"), {
             method: "POST",
             body: JSON.stringify(todo),
             headers: {
-                "Content-type": "application/json; charset=UTF-8",
+                "Content-type": "application/json",
+                Accept: "application/json",
             },
         }).then((response) => response.json());
 
-    const deleteTodo = (id) =>
-        fetch([baseurl, path, id].join("/"), { method: "DELETE" });
+
+    // const deleteTodo = (id) =>
+    //     fetch([baseurl, path, id].join("/"), { method: "DELETE" });
 
     return {
         getTodos,
-        deleteTodo,
+        // deleteTodo,
         addTodo,
     };
 })();
 
+//set a list boolean value, once click the add button it will turn to true
+let input_add = false
 
 const View = (() => {
     const domstr = {
         eventlist: "#table_eventlist",
-        deletebtn: ".delete_btn",
+        addbtn: ".add_new",
     };
     const render = (element, tmp) => {
-        element.innerHTML = tmp;
+ 
+            element.innerHTML = tmp;
+     
+        
     };
 
-  
+
     const createTmp = (arr) => {
         let tmp = ` 
             <tr class="table__head">
@@ -47,21 +53,21 @@ const View = (() => {
             </tr>`;
         arr.forEach((ele) => {
 
-                //startDate          
-                const time = parseInt(ele["startDate"])
-                const all_date = new Date(time);
-                const year = all_date.getFullYear()
-                const month = all_date.getMonth()+1
-                const date = all_date.getDate()
+            //startDate          
+            const time = parseInt(ele["startDate"])
+            const all_date = new Date(time);
+            const year = all_date.getFullYear()
+            const month = all_date.getMonth() + 1
+            const date = all_date.getDate()
 
-                //endDate
-                const timeEnd = parseInt(ele["endDate"])
-                const all_date_end = new Date(timeEnd);
-                const End_year = all_date_end.getFullYear()
-                const End_month = all_date_end.getMonth()+1
-                const End_date = all_date_end.getDate()
-           
-      
+            //endDate
+            const timeEnd = parseInt(ele["endDate"])
+            const all_date_end = new Date(timeEnd);
+            const End_year = all_date_end.getFullYear()
+            const End_month = all_date_end.getMonth() + 1
+            const End_date = all_date_end.getDate()
+
+            
             tmp += `
                
             <tr class="table__content"> 
@@ -72,7 +78,7 @@ const View = (() => {
                     <input disabled value="${year}-${month}-${date}" />
                 </td>
                 <td>
-                    <input disabled  value="${End_year}-${End_month}-${End_date}" />
+                    <input disabled value="${End_year}-${End_month}-${End_date}" />
                 </td>
                 <td>
                     <button class="edit_button" id="${ele.id}" >EDIT</button>
@@ -80,7 +86,29 @@ const View = (() => {
                 </td>
             </tr>
             `;
+
         });
+       
+            tmp+=`
+               
+            <tr> 
+                <td>
+                    <input value=''} />
+                </td>
+                <td>
+                    <input value="" />
+                </td>
+                <td>
+                    <input value="" />
+                </td>
+                <td>
+                    <button>SAVE</button>
+                    <button>CLOSE</button>
+                </td>
+            </tr>
+            `;
+       
+
         return tmp;
     };
 
@@ -91,13 +119,15 @@ const View = (() => {
     };
 })();
 
+
+
 // ~~~~~~~~~~~~~~~model~~~~~~~~~~~~~~~~~~~~~
 const Model = ((api, view) => {
     class Todo {
-        constructor(title) {
-            this.userId = 20;
-            this.title = title;
-            this.completed = false;
+        constructor(eventName, startDate, endDate) {
+            this.eventName = eventName;
+            this.startDate = startDate;
+            this.endDate = endDate;
         }
     }
 
@@ -126,12 +156,12 @@ const Model = ((api, view) => {
 
     const getTodos = api.getTodos;
     // const deleteTodo = api.deleteTodo;
-    // const addTodo = api.addTodo;
+    const addTodo = api.addTodo;
 
     return {
         Todo,
         State,
-        // addTodo,
+        addTodo,
         getTodos,
         // deleteTodo,
     };
@@ -141,19 +171,16 @@ const Model = ((api, view) => {
 const Controller = ((model, view) => {
     const state = new model.State();
 
-    // const addTodo = () => {
-    //     const inputbox = document.querySelector(view.domstr.inputbox);
-
-    //     inputbox.addEventListener("keyup", (event) => {
-    //         if (event.key === "Enter") {
-    //             const todo = new model.Todo(event.target.value);
-    //             model.addTodo(todo).then((newtodo) => {
-    //                 state.todolist = [newtodo, ...state.todolist];
-    //                 event.target.value = "";
-    //             });
-    //         }
-    //     });
-    // };
+    //Add list
+    const addTodo = () => {
+        const addbutton = document.querySelector(view.domstr.addbtn)
+        addbutton.addEventListener("click", (event) => {
+            const todo = new model.Todo('', '', '');
+            model.addTodo(todo).then((newtodo) => {
+                state.todolist = [ ...state.todolist,newtodo];
+            });
+        })
+    }
 
     // const deletTodo = () => {
     //     const ele = document.querySelector(view.domstr.todolist);
@@ -178,7 +205,7 @@ const Controller = ((model, view) => {
     const bootstrap = () => {
         init();
         // deletTodo();
-        // addTodo();
+        addTodo();
     };
 
     return { bootstrap };
