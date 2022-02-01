d5a3d4afc4b827ab1d9b5f0d9b6c25f707276d1a
Xiang Liu <jane@Xiangs-MacBook-Pro.local>
2022-02-01 12:40:20 -0800

    Refine Code

--- EventListApp/script.js
+++ EventListApp/script.js
@@ -1,5 +1,6 @@
 const eventList = document.getElementById('table_eventlist')
-let output=`
+const addnew = document.querySelector('.add_new')
+let output = `
         <tr class="table__head">
         <th>Event name</th>
         <th>Start date</th>
@@ -8,16 +9,13 @@ let output=`
         </tr>
         `
 const url = 'http://localhost:3000/events'
-let eventLen;
-const eventLenf = (events)=>{
-    return events.length
-}
-const renderEvents = (events)=>{
-    events.forEach(ele=> {
+
+const renderEvents = (events) => {
+    events.forEach(ele => {
         let startdate = timeConverter(ele["startDate"])
-        let enddate =timeConverter(ele["endDate"])
-        output+=`
-            <tr class="table__content"> 
+        let enddate = timeConverter(ele["endDate"])
+        output += `
+            <tr class="table__content" id=${ele["id"]}> 
                 <td>
                     <input disabled value=${ele["eventName"]} />
                 </td>
@@ -28,14 +26,14 @@ const renderEvents = (events)=>{
                     <input disabled value=${enddate} />
                 </td>
                 <td>
-                    <button class="edit_button" id="${ele.id}" >EDIT</button>
-                    <button id="${ele.id}">DELETE</button>
+                    <button class="edit_button" id="edit-event" >EDIT</button>
+                    <button id="delete-event">DELETE</button>
                 </td>
             </tr>
         `
     });
 
-    output+=` 
+    output += ` 
         <tr> 
             <td>
                 <input type="text" value="" id="eventName_${events.length}" />
@@ -48,21 +46,43 @@ const renderEvents = (events)=>{
             </td>
             <td>
                 <button class="save" id='save_${events.length}' onclick='save(event)'}>SAVE</button>
-                <button>CLOSE</button>
+                <button id="close-event">CLOSE</button>
             </td>
         </tr>`
-    
-    eventList.innerHTML=output
+
+    eventList.innerHTML = output
 
 }
 
+//Add new row click
+addnew.addEventListener('click', (events) => {
+    output += ` 
+        <tr> 
+            <td>
+                <input type="text" value="" id="eventName_${events.length}" />
+            </td>
+            <td>
+                <input type="date" value="" id="startDate_${events.length}" />
+            </td>
+            <td>
+                <input type="date" value="" id="endDate_${events.length}" />
+            </td>
+            <td>
+                <button class="save" id='save_${events.length}' onclick='save(event)'}>SAVE</button>
+                <button id="close-event">CLOSE</button>
+            </td>
+        </tr>`
+    eventList.innerHTML = output
+})
 
-const timeConverter = (timestamp)=>{
+
+
+const timeConverter = (timestamp) => {
     const timestamp_int = parseInt(timestamp)
     const all_date = new Date(timestamp_int);
     const year = all_date.getFullYear()
-    const month = (all_date.getMonth() + 1) < 10 ?  (all_date.getMonth() + 1).toString().padStart(2, '0'): (all_date.getMonth() + 1)
-    const date = all_date.getDate() < 10 ? all_date.getDate().toString().padStart(2, '0'): all_date.getDate()
+    const month = (all_date.getMonth() + 1) < 10 ? (all_date.getMonth() + 1).toString().padStart(2, '0') : (all_date.getMonth() + 1)
+    const date = all_date.getDate() < 10 ? all_date.getDate().toString().padStart(2, '0') : all_date.getDate()
     return `${year}-${month}-${date}`
 }
 
@@ -72,44 +92,63 @@ const timeConverter = (timestamp)=>{
 //Get - Read the posts
 let res
 fetch(url)
-    .then(res=>res.json())
-    .then(data=>{
-        renderEvents(data) 
+    .then(res => res.json())
+    .then(data => {
+        renderEvents(data)
     })
-   
-//Insert New Post
-//Timestamp convert
 
-// function toTimestamp(year,month,day,hour,minute,second){
-//     var datum = new Date(Date.UTC(year,month-1,day,hour,minute,second));
-//     return datum.getTime()/1000;
-//    }
+eventList.addEventListener('click', (e) => {
+    e.preventDefault();
+    let delButtonIsPressed = e.target.id === 'delete-event'
+    let editButtonIsPressed = e.target.id === 'edit-event'
+    let id = e.target.parentElement.parentElement.id
 
-function toTimestamp(strDate){
+    //Delete - Remove the existing event
+    //method: DELETE
+    if (delButtonIsPressed) {
+        fetch([url, id].join('/'), {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            })
+            .then((response) => response.json())
+            .then((json) => console.log(json));
+        console.log('delete')
+    }
+
+    if(editButtonIsPressed){
+        
+    }
+
+})
+
+function toTimestamp(strDate) {
     var datum = Date.parse(strDate);
-    return datum/1000;
- }
+    return datum / 1000;
+}
 
-function save(event){
-    
+function save(event) {
+
     const len = event.target.id.split('_')[1]
     const start_date = document.getElementById(`startDate_${len}`).value.split('-')
- 
+
     const eve_name = document.getElementById(`eventName_${len}`).value
     const start_yy = parseInt(start_date[2])
-    const start_mm= parseInt(start_date[0])<10 ? start_date[0].padStart(2, '0'): parseInt(start_date[0])
-    const start_dd= parseInt(start_date[1])<10 ? start_date[1].padStart(2,'0'): parseInt(start_date[1])
+    const start_mm = parseInt(start_date[0]) < 10 ? start_date[0].padStart(2, '0') : parseInt(start_date[0])
+    const start_dd = parseInt(start_date[1]) < 10 ? start_date[1].padStart(2, '0') : parseInt(start_date[1])
 
     const end_date = document.getElementById(`endDate_${len}`).value.split('-')
     const end_yy = parseInt(end_date[2])
-    const end_mm = parseInt(end_date[0])<10 ? end_date[0].padStart(2, '0'): parseInt(end_date[0])
-    const end_dd = parseInt(end_date[1])<10 ? end_date[1].padStart(2,'0'): parseInt(end_date[1])
+    const end_mm = parseInt(end_date[0]) < 10 ? end_date[0].padStart(2, '0') : parseInt(end_date[0])
+    const end_dd = parseInt(end_date[1]) < 10 ? end_date[1].padStart(2, '0') : parseInt(end_date[1])
 
     // console.log(toTimestamp('02/13/2020 23:31:30'));
-    const start_time=`${start_mm}/${start_dd}/${start_yy} 23:31:30`
-    const end_time=`${end_mm}/${end_dd}/${end_yy} 23:31:30`
+    const start_time = `${start_mm}/${start_dd}/${start_yy} 23:31:30`
+    const end_time = `${end_mm}/${end_dd}/${end_yy} 23:31:30`
 
-    const start_timestamp= toTimestamp(start_time)
+    const start_timestamp = toTimestamp(start_time)
     const end_timestamp = toTimestamp(end_time)
 
 
@@ -124,6 +163,11 @@ function save(event){
             startDate: start_timestamp,
             endDate: end_timestamp,
         }),
-        })
+    })
         .then((response) => response.json())
+        .then(data => {
+            const dataArr = []
+            dataArr.push(data)
+            renderEvents(dataArr)
+        })
 }
