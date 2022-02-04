4626ea637e2731ea5e147941424fd744e58db5c7
Xiang Liu <jane@Xiangs-MacBook-Pro.local>
2022-02-03 16:45:25 -0800

    Add Pagination

--- EventListApp/script.js
+++ EventListApp/script.js
@@ -3,53 +3,119 @@ const addnew = document.querySelector('.add_new')
 let output = ""
 const url = 'http://localhost:3000/events'
 
+let pagination = document.getElementById("pagination")
+const prev = document.getElementById("PREV")
+const next = document.getElementById("NEXT")
+let pageNum = document.getElementById('pageNumber')
+
+let currentPage = 0
+
 //Show data
 const renderEvents = (events) => {
-    events.forEach(ele => {
-        let startdate = timeConverter(ele["startDate"])
-        let enddate = timeConverter(ele["endDate"])
-        output += `
-            <tr class="table__content" id=${ele["id"]}> 
-                <td>
-                    <input disabled value=${ele["eventName"]} id="eventName_${ele["id"]}" />
-                </td>
-                <td>
-                    <input disabled value=${startdate} id="startDate_${ele["id"]}" />
-                </td>
-                <td>
-                    <input disabled value=${enddate} id="endDate_${ele["id"]}" />
-                </td>
-                <td>
-                    <input type="submit" class="edit_button" class="edit-event_${ele["id"]}" id="edit-event" value="EDIT">
-                    <input type="submit" id="delete-event" value="DELETE">
-                </td>
-            </tr>
-        `
-    });
 
-//Add extra empty input 
-    let len = events.length + 1
-    output += ` 
-        <tr id='Input_${len}''> 
-            <td>
-                <input type="text" value="" id="eventName_${len}" />
-            </td>
-            <td>
-                <input type="date" value="" id="startDate_${len}" />
-            </td>
-            <td>
-                <input type="date" value="" id="endDate_${len}" />
-            </td>
-            <td>
-                <input type="submit" class="save" id="save_${len}" value="SAVE">
-                <input type="submit" id="close_${len}" value="CLOSE">
-            </td>
-        </tr>`
+    //Define totalPage number
+    let totalPage = Math.ceil(events.length / 4)
+    if (currentPage === 0) {
+        events.slice(0, 4).forEach(ele => {
+            let startdate = timeConverter(ele["startDate"])
+            let enddate = timeConverter(ele["endDate"])
+            output += `
+                <tr class="table__content" id=${ele["id"]}> 
+                    <td>
+                        <input disabled value=${ele["eventName"]} id="eventName_${ele["id"]}" />
+                    </td>
+                    <td>
+                        <input disabled value=${startdate} id="startDate_${ele["id"]}" />
+                    </td>
+                    <td>
+                        <input disabled value=${enddate} id="endDate_${ele["id"]}" />
+                    </td>
+                    <td>
+                        <input type="submit" class="edit_button" class="edit-event_${ele["id"]}" id="edit-event" value="EDIT">
+                        <input type="submit" id="delete-event" value="DELETE">
+                    </td>
+                </tr>
+            `
+            eventList.innerHTML = output
+        })
+        pageNum.innerHTML = `1/${totalPage}`
+    }
 
-    eventList.innerHTML = output
+    pagination.addEventListener('click', (e) => {
+        let nextButtonIsPressed = e.target.value === 'NEXT'
+        let prevButtonIsPressed = e.target.value === 'PREV'
+        let counter
 
+        //NextButtonIsPressed
+        if (nextButtonIsPressed) {
+            output = ``
+            currentPage += 4
+            if (currentPage <= events.length) {
+                events.slice(currentPage, currentPage + 4).forEach(ele => {
+                    let startdate = timeConverter(ele["startDate"])
+                    let enddate = timeConverter(ele["endDate"])
+                    output += `
+                    <tr class="table__content" id=${ele["id"]}> 
+                        <td>
+                            <input disabled value=${ele["eventName"]} id="eventName_${ele["id"]}" />
+                        </td>
+                        <td>
+                            <input disabled value=${startdate} id="startDate_${ele["id"]}" />
+                        </td>
+                        <td>
+                            <input disabled value=${enddate} id="endDate_${ele["id"]}" />
+                        </td>
+                        <td>
+                            <input type="submit" class="edit_button" class="edit-event_${ele["id"]}" id="edit-event" value="EDIT">
+                            <input type="submit" id="delete-event" value="DELETE">
+                        </td>
+                    </tr>
+                `
+                    eventList.innerHTML = output
+                })
+
+
+            }
+        }
+
+        //PrevButtonIsPressed
+        if (prevButtonIsPressed) {
+            output = ``
+            currentPage -= 4
+            if (currentPage >= 0) {
+                events.slice(currentPage, currentPage + 4).forEach(ele => {
+                    let startdate = timeConverter(ele["startDate"])
+                    let enddate = timeConverter(ele["endDate"])
+                    output += `
+                 <tr class="table__content" id=${ele["id"]}> 
+                     <td>
+                         <input disabled value=${ele["eventName"]} id="eventName_${ele["id"]}" />
+                     </td>
+                     <td>
+                         <input disabled value=${startdate} id="startDate_${ele["id"]}" />
+                     </td>
+                     <td>
+                         <input disabled value=${enddate} id="endDate_${ele["id"]}" />
+                     </td>
+                     <td>
+                         <input type="submit" class="edit_button" class="edit-event_${ele["id"]}" id="edit-event" value="EDIT">
+                         <input type="submit" id="delete-event" value="DELETE">
+                     </td>
+                 </tr>
+             `
+                    eventList.innerHTML = output
+                })
+
+            }
+        }
+        
+        //Define counter and totalPage
+        counter = Math.floor((currentPage / 4) + 1)
+        if (counter >= 1 && counter <= totalPage) {
+            pageNum.innerHTML = `${counter}/${totalPage}`
+        }
+    })
 }
-
 //AddNewButton to add new input
 addnew.addEventListener('click', (events) => {
     let len = events.length + 1
@@ -103,17 +169,17 @@ function toTimestamp(strDate) {
 //Define All Methods
 
 //Get - Read the posts
-const getList=(url)=>{
+const getList = (url) => {
     fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        renderEvents(data)
-    })
+        .then(res => res.json())
+        .then(data => {
+            renderEvents(data, currentPage)
+        })
 
 }
 
 //Post
-const createList=(url, eventName, startTimeStamp, endTimeStamp)=>{
+const createList = (url, eventName, startTimeStamp, endTimeStamp) => {
     fetch(url, {
         method: "POST",
         headers: {
@@ -134,7 +200,7 @@ const createList=(url, eventName, startTimeStamp, endTimeStamp)=>{
 
 
 //Delete
-const deleteList=(url, id)=>{
+const deleteList = (url, id) => {
     fetch([url, id].join('/'), {
         method: "DELETE",
         headers: {
@@ -147,7 +213,7 @@ const deleteList=(url, id)=>{
 
 
 //Update
-const updateList=(url, id, eventName, startTimeStamp, endTimeStamp)=>{
+const updateList = (url, id, eventName, startTimeStamp, endTimeStamp) => {
     fetch([url, id].join("/"), {
         method: "PUT",
         headers: {
@@ -168,27 +234,7 @@ const updateList=(url, id, eventName, startTimeStamp, endTimeStamp)=>{
 //Call Get Event
 getList(url)
 
-//Call Post Event
-// function save(event) {
-//     const save_id = event.target.id.split('_')[1]
-//     const start_date = document.getElementById(`startDate_${save_id}`).value
-//     const end_date = document.getElementById(`endDate_${save_id}`).value
-
-//     const start_timestamp = toTimestamp(start_date)
-//     const end_timestamp = toTimestamp(end_date)
-
-//     const eve_name = document.getElementById(`eventName_${save_id}`).value
-
-//     //POST Event
-//     if (eve_name !== '' &&
-//         start_timestamp !== '' &&
-//         end_timestamp !== '') {
-//             createList(url, eve_name, start_timestamp, end_timestamp)
-//     } else {
-//         alert("Enter valid value")
-//     }
-// }
-
+//Define all eventList listener
 eventList.addEventListener('click', (e) => {
     let id = e.target.parentElement.parentElement.id
     let delButtonIsPressed = e.target.value === 'DELETE'
@@ -204,20 +250,20 @@ eventList.addEventListener('click', (e) => {
     const UpdateEndDay = document.getElementById(`endDate_${id}`)
 
     //Post Event
-    if(saveButtonIsPressed){
+    if (saveButtonIsPressed) {
         const save_id = e.target.id.split('_')[1]
         const start_date = document.getElementById(`startDate_${save_id}`).value
         const end_date = document.getElementById(`endDate_${save_id}`).value
         const eve_name = document.getElementById(`eventName_${save_id}`).value
-    
+
         const start_timestamp = toTimestamp(start_date)
         const end_timestamp = toTimestamp(end_date)
-    
-    
-    //POST Event
-    if (eve_name !== '' &&
-        start_timestamp !== '' &&
-        end_timestamp !== '') {
+
+
+        //POST Event
+        if (eve_name !== '' &&
+            start_timestamp !== '' &&
+            end_timestamp !== '') {
             createList(url, eve_name, start_timestamp, end_timestamp)
         } else {
             alert("Please enter valid values to all fields")
@@ -268,6 +314,5 @@ eventList.addEventListener('click', (e) => {
     }
 
 })
-
 
 
